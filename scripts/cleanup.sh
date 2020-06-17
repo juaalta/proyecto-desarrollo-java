0afca514437a083c2bdbabdf29cc4b1f67171680
JUAN SALVADOR ALEIXANDRE TALENS <juansalvador.aleixandre@go-aigua.com>
2020-06-18 00:59:32 +0200

    Added scripts work with docker.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,6 +9,9 @@ DOCKER=$(which docker)
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 echo "[$(date +"%Y-%m-%d %T")] Stopping and removing all the containers..."
+# echo $PROJECT_PATH
+# echo $PWD
+# echo $HOME
 
 ${DOCKER} stop $(${DOCKER} ps -a -q) && ${DOCKER} rm $(${DOCKER} ps -a -q) --volumes
 
--- scripts/mvn_cmd.sh
+++ scripts/mvn_cmd.sh
@@ -9,5 +9,8 @@ DOCKER=$(which docker)
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 echo "[$(date +"%Y-%m-%d %T")] Executing the mvn command: $1"
+# echo $PROJECT_PATH
+# echo $PWD
+# echo $HOME
 
 ${DOCKER} run -it --rm --name my-maven-project -v "$PWD"/.:/usr/src/app -v "$HOME"/.m2:/root/.m2 -w /usr/src/app maven:3.6.3-jdk-11 mvn $1
