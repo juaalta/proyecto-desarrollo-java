dd3ce847b777acf9da358cd13b40b1a2d75a397f
JUAN SALVADOR ALEIXANDRE TALENS <juansalvador.aleixandre@go-aigua.com>
2020-06-19 00:27:03 +0200

    The name of the project is used as the container name.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,11 +7,13 @@ DOCKER=$(which docker)
 ####################
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+PROJECT_NAME="$(cd $PROJECT_PATH && ./scripts/app_name.sh)"
 
 echo "[$(date +"%Y-%m-%d %T")] Stopping and removing all the containers..."
 # echo $PROJECT_PATH
 # echo $PWD
 # echo $HOME
+# echo $PROJECT_NAME
 
 ${DOCKER} stop $(${DOCKER} ps -a -q) && ${DOCKER} rm $(${DOCKER} ps -a -q) --volumes
 
--- scripts/mvn_cmd.sh
+++ scripts/mvn_cmd.sh
@@ -7,10 +7,12 @@ DOCKER=$(which docker)
 ####################
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+PROJECT_NAME="$(cd $PROJECT_PATH && ./scripts/app_name.sh)"
 
 echo "[$(date +"%Y-%m-%d %T")] Executing the mvn command: $1"
 # echo $PROJECT_PATH
 # echo $PWD
 # echo $HOME
+# echo $PROJECT_NAME
 
-${DOCKER} run -it --rm --name my-maven-project -v "$PWD"/.:/usr/src/app -v "$HOME"/.m2:/root/.m2 -w /usr/src/app maven:3.6.3-jdk-11 mvn $1
+${DOCKER} run -it --rm --name $PROJECT_NAME -v "$PWD"/.:/usr/src/app -v "$HOME"/.m2:/root/.m2 -w /usr/src/app maven:3.6.3-jdk-11 mvn $1
