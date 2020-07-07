445adda63ed9b9e8bfa787891ab31b156816a221
JUAN SALVADOR ALEIXANDRE TALENS <juansalvador.aleixandre@go-aigua.com>
2020-07-08 00:28:03 +0200

    Changed the name of the package.

--- src/main/java/com/juaalta/demo/DemoApplication.java
+++ src/main/java/com/juaalta/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.idrica.goaigua.demo;
+package com.juaalta.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/juaalta/demo/DemoApplicationTests.java
+++ src/test/java/com/juaalta/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.idrica.goaigua.demo;
+package com.juaalta.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
